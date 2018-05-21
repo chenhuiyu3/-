cf32403e4c753312aec8084cbdb4ce5b9fa351db
chenhuiyu3 <chenhuiyu3@139.com>
2018-05-21 11:06:12 +0800

    first build the repository

---
+++ app1/src/main/java/com/example/gmcc/hellowmyfirst/DevicActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gmcc.hellowmyfirst
+
+import android.app.Activity
+import android.os.Bundle
+
+class DevicActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_devic)
+    }
+}
